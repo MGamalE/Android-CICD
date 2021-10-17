eec60a8ad2a974af9d2e9c3b9cc5a003734f571e
mohamedelsharkawy <Mohamedgamal@fci.helwan.edu.eg>
2021-10-17 11:45:52 +0200

    Init commit

---
+++ app/src/main/java/com/sharkawy/android_cicd/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sharkawy.android_cicd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
