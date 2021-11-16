9badc9321dff512f65d519652d7da030aba558c4
mohamedgelsharkawy <mohammedgammal96@gmail.com>
2021-11-16 17:03:05 +0200

    Update Activity

--- app/src/main/java/com/sharkawy/android_cicd/MainActivity.kt
+++ app/src/main/java/com/sharkawy/android_cicd/MainActivity.kt
@@ -3,6 +3,7 @@ package com.sharkawy.android_cicd
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.common.util.Strings
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,4 +14,6 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "Hey! Firebase distributions integrated successfully! :)", Toast.LENGTH_SHORT).show()
 
     }
+
+
 }
